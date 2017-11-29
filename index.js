61551c0e1f1da09674275aeec91143907b4720da
Daniel L <theusafyt@gmail.com>
2017-11-29 19:45:30 +0000

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ window.onload = function(){
   
   var ref = firebase.database().ref();
   
-  var version = "1.1.4";
+  var version = "1.1.5";
   var v = document.getElementById("version");
   v.innerText = "v. " + version;
   
