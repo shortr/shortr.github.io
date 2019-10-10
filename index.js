e0660f0adeca708b15895d964da1e2c91c210957
theusaf <32113157+theusaf@users.noreply.github.com>
2019-10-10 13:47:53 -0700

    Update code to use REST API
    Not using WebSocket, should increase speed.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//version 1.5.1
+//version 1.7.0
 /*global firebase*/
 window.onload = function(){
   var config = {
@@ -13,7 +13,7 @@ window.onload = function(){
   
   var ref = firebase.database().ref();
   
-  var version = "1.6.0";
+  var version = "1.7.0";
   var v = document.getElementById("version");
   v.innerText = "v. " + version;
   
@@ -68,9 +68,12 @@ window.onload = function(){
   
   bu.onclick = function(){
       var n = 0;
-      ref.once("value", function(snapshot) {
-          n = snapshot.numChildren();
-          var l = snapshot.val();
+      let x= new XMLHttpRequest();
+      x.open("GET","https://rickroll-4b28d.firebaseio.com/.json");
+      x.send();
+      x.onload = function() {
+          n = Object.keys(JSON.parse(x.response)).length;
+          var l = JSON.parse(x.response);
           var exists = false;
           for(var i in l){
             if(l[i] === linksh.value){
@@ -86,7 +89,7 @@ window.onload = function(){
           if(exists === false){
             conv(n + 1);
           }
-      });
+      };
   };
   
   if(window.location.href.split("?")[1] === "_c9_id=livepreview3&_c9_host=https://ide.c9.io" || window.location.href.split("?")[1] === "_c9_id=livepreview0&_c9_host=https://ide.c9.io" || window.location.href.split("?")[1] === "_c9_id=livepreview1&_c9_host=https://ide.c9.io" || window.location.href.split("?")[1] === "" || window.location.href.split("?")[1] === undefined || window.location.href.split("?")[1] === null){
@@ -99,43 +102,6 @@ window.onload = function(){
       var hi = window.location.href.split("?");
       delete hi[0];
       hi = hi.join().replace(/,/img,"");
-/*      ref.once('value',function(s){
-          if(s.hasChild(hi) == true){
-              ref.child(hi).once('value',function(url){
-                var k = url.val().replace(/"/,"%22");
-                k = url.val().replace(/'/,"%27");
-                document.write("<h1>Click the window to continue to your link</h1>");
-                document.write("<h4>if it doesn't work, click this link:" + "<a href='" + k + "' target=_blank onclick=window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')>" + "Click Me!" + "</a></h4><textarea width='100%' style='width: 100%'>"+k+"</textarea>");
-                window.onmousedown = function(){
-                  if(url.val().search("://") === -1){
-                    if(url.val().search("data:") === -1){
-                      window.open("https://" + url.val(),"_blank");
-                    }else{
-                      var newWin = window.open("","_blank");
-                      if(url.val().substr(0,10).search("img") != -1 || url.val().substr(0,13).search("image") != -1){
-                        newWin.document.write("<img src='" + k + "'/>");
-                      }else if(url.val().substr(0,13).search("audio") != -1){
-                        newWin.document.write("<audio src='" + k + "'/>");
-                      }else if(url.val().substr(0,13).search("video") != -1){
-                        newWin.document.write("<video src='" + k + "'/>");
-                      }else{
-                        newWin.document.write("<iframe src='" + k + "' frameBorder=0 width=100% height=100%>");
-                      }
-                    }
-                  }else{
-                    window.open(url.val(),"_blank");
-                  }
-                  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-                };
-                if(window.ontouchstart){
-                  window.ontouchstart = window.onmousedown;
-                }
-              });
-          }else{
-              alert("Error 404 - URL not found");
-              window.location.replace("https://shortr.github.io");
-          }
-      });*/ //Old version
       //faster loading
       ref.child(hi).once("value",function(url){
         if(url.val() !== null){
