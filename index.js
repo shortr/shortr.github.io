f95c34d2a9b2f683689296430dc34a83e6d22daa
theusaf <theusafyt@gmail.com>
2020-01-29 16:12:13 -0800

    Fomatting, styling, faster stuff

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//version 1.7.0
+//version 2.0.0
 /*global firebase*/
 window.onload = function(){
   var config = {
@@ -10,37 +10,36 @@ window.onload = function(){
     messagingSenderId: "836606014111"
   };
   firebase.initializeApp(config);
-  
+
   var ref = firebase.database().ref();
-  
-  var version = "1.7.0";
+
+  var version = "2.0.0";
   var v = document.getElementById("version");
   v.innerText = "v. " + version;
-  
+
   var linksh = document.getElementById("mainlink");
   var bu = document.getElementById("submit");
   var displaytext = document.getElementById("redirlink");
-  
+
   var randChars = ["a","b","c","d","e","f"];
   var randChars2 = ["A","B","C","D","E","F"];
   var randChars3 = ["1","2","3","4","5","6"];
-  
-  //converts numbers to chars. beware using custom chars for links, they can be overwritten more easily
+
   function conv(num){
     var nn = num;
     var nn2 = nn;
     var rep = true;
     var string = ""; //string of shortlink
     var l = 0; //length of string
-    
+
     while (rep==true){
       if(nn2 >= 342){
-          nn = nn2;
-          nn2 = nn - 342;
-          nn = 342;
+        nn = nn2;
+        nn2 = nn - 342;
+        nn = 342;
       }else{
-          nn = nn2;
-          rep = false;
+        nn = nn2;
+        rep = false;
       }
       if(Math.floor(nn/49)-1 >= 0){
         string = string + randChars3[Math.floor(nn/49)-1];
@@ -54,95 +53,133 @@ window.onload = function(){
         string = string + randChars[nn - 1];
       }
       if(rep == true){
-          //nope
+        //nope
       }else{
         ref.child(string).set(linksh.value);
         let clink = linksh.value;
         if(linksh.value.length >= 60){
           clink = linksh.value.substr(0,60) + "...";
         }
-        displaytext.innerText = "Success! Your link leads to " + clink + ": https://shortr.github.io/?" + string;
+        displaytext.innerHTML = "<p>Success! Your link leads to " + clink + ":<br><a href=\"https://shortr.github.io/?" + string + "\">https://shortr.github.io/?" + string + "</a></p>";
       }
     }
   }
-  
-  bu.onclick = function(){
-      var n = 0;
-      let x= new XMLHttpRequest();
-      x.open("GET","https://rickroll-4b28d.firebaseio.com/.json");
-      x.send();
-      x.onload = function() {
-          n = Object.keys(JSON.parse(x.response)).length;
-          var l = JSON.parse(x.response);
-          var exists = false;
-          for(var i in l){
-            if(l[i] === linksh.value){
-              let clink = linksh.value;
-              if(linksh.value.length >= 60){
-                clink = linksh.value.substr(0,60) + "...";
-              }
-              displaytext.innerText = "Success! Your link leads to " + clink + ": https://shortr.github.io/?" + i;
-              exists = true;
-              break;
-            }
+
+  function send(){
+    displaytext.innerHTML = "<p>Loading... (0%)</p>";
+    var n = 0;
+    let x= new XMLHttpRequest();
+    x.open("GET","https://rickroll-4b28d.firebaseio.com/.json");
+    x.send();
+    x.onload = function() {
+      n = Object.keys(JSON.parse(x.response)).length;
+      var l = JSON.parse(x.response);
+      var exists = false;
+      for(var i in l){
+        if(l[i] === linksh.value){
+          let clink = linksh.value;
+          if(linksh.value.length >= 60){
+            clink = linksh.value.substr(0,60) + "...";
           }
-          if(exists === false){
-            conv(n + 1);
-          }
-      };
+          displaytext.innerHTML = "<p>Success! Your link leads to " + clink + ":<br><a href=\"https://shortr.github.io/?" + i + "\">https://shortr.github.io/?" + i + "</a></p>";
+          exists = true;
+          break;
+        }
+      }
+      if(exists === false){
+        conv(n + 1);
+      }
+    };
+    x.onprogress = function(e) {
+      displaytext.innerHTML = `<p>Loading... (${((e.loaded/e.total)*100).toFixed(1)}%)</p>`;
+    };
   };
-  
-  if(window.location.href.split("?")[1] === "_c9_id=livepreview3&_c9_host=https://ide.c9.io" || window.location.href.split("?")[1] === "_c9_id=livepreview0&_c9_host=https://ide.c9.io" || window.location.href.split("?")[1] === "_c9_id=livepreview1&_c9_host=https://ide.c9.io" || window.location.href.split("?")[1] === "" || window.location.href.split("?")[1] === undefined || window.location.href.split("?")[1] === null){
-      //donothing
-        setTimeout(function(){
-        document.getElementById("con").style.display = "block";
-        },2000);
+
+  linksh.addEventListener("keydown",e=>{
+    if(e.key == "Enter"){
+      send();
+    }
+  });
+
+  bu.onclick = send;
+
+  if(!location.href.split("?")[1]){
+    //donothing
+    document.getElementById("con").style.display = "block";
   }else{
-      //redirect
-      var hi = window.location.href.split("?");
-      delete hi[0];
-      hi = hi.join().replace(/,/img,"");
-      //faster loading
-      ref.child(hi).once("value",function(url){
-        if(url.val() !== null){
-          var k = url.val().replace(/"/,"%22");
-          k = url.val().replace(/'/,"%27");
-          document.write("<h1>Click the window to continue to your link</h1>");
-          document.write("<h4>if it doesn't work, click this link:" + "<a href='" + k + "' target=_blank onclick=window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')>" + "Click Me!" + "</a></h4><textarea width='100%' style='width: 100%'>"+k+"</textarea>");
-          window.onmousedown = function(){
-            if(url.val().search("://") === -1){
-              if(url.val().search("data:") === -1){
-                window.open("https://" + url.val(),"_blank");
-                }else{
-                  var newWin = window.open("","_blank");
-                  if(url.val().substr(0,10).search("img") != -1 || url.val().substr(0,13).search("image") != -1){
-                  newWin.document.write("<img src='" + k + "'/>");
-                    }else if(url.val().substr(0,13).search("audio") != -1){
-                    newWin.document.write("<audio src='" + k + "'/>");
-                  }else if(url.val().substr(0,13).search("video") != -1){
-                    newWin.document.write("<video src='" + k + "'/>");
-                  }else if(url.val().substr(0,14).search("html") != -1){
-                    try{
-                      newWin.document.write(atob(url.val().split(",").slice(1).join(",")));
-                    }catch(err){
-                      newWin.document.write(url.val().split(",").slice(1).join(","));
-                    }
-                  }else{
-                    newWin.document.write("<iframe src='" + k + "' frameBorder=0 width=100% height=100%>");
-                  }
+    //redirect
+    var hi = window.location.href.split("?");
+    delete hi[0];
+    hi = hi.join().replace(/,/img,"");
+    //faster loading
+    ref.child(hi).once("value",function(url){
+      if(url.val() !== null){
+        var k = url.val().replace(/"/,"%22");
+        k = url.val().replace(/'/,"%27");
+        document.write("<h1>Click the window to continue to your link</h1>");
+        document.write("<h4>if it doesn't work, click this link:" + "<a href='" + k + "' target=_blank onclick=window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')>" + "Click Me!" + "</a></h4><textarea width='100%' style='width: 100%'>"+k+"</textarea>");
+        rhead();
+        window.onmousedown = function(){
+          if(url.val().search("://") == -1){
+            if(url.val().search("data:") == -1){
+              // if not even a url at all
+              if(url.val().search("\\.") == -1){
+                var w = window.open("","_blank");
+                w.document.write(url.val());
+                return;
+              }
+              window.open("https://" + url.val(),"_blank");
+            }else{
+              // if was a data: thing
+              var newWin = window.open("","_blank");
+              if(url.val().substr(0,10).search("img") != -1 || url.val().substr(0,13).search("image") != -1){
+                newWin.document.write("<img src='" + k + "'/>");
+              }else if(url.val().substr(0,13).search("audio") != -1){
+                newWin.document.write("<audio src='" + k + "'/>");
+              }else if(url.val().substr(0,13).search("video") != -1){
+                newWin.document.write("<video src='" + k + "'/>");
+              }else if(url.val().substr(0,14).search("html") != -1){
+                try{
+                  newWin.document.write(atob(url.val().split(",").slice(1).join(",")));
+                }catch(err){
+                  newWin.document.write(url.val().split(",").slice(1).join(","));
                 }
               }else{
-                window.open(url.val(),"_blank");
+                newWin.document.write("<iframe src='" + k + "' frameBorder=0 width=100% height=100%>");
               }
-              window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-            };
-            if(window.ontouchstart){
-            window.ontouchstart = window.onmousedown;
+            }
+          }else{
+            window.open(url.val(),"_blank");
           }
-        }else{
-          alert("Error 404 - URL not found");
-          window.location.replace("https://shortr.github.io");
+          window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        };
+        if(window.ontouchstart){
+          window.ontouchstart = window.onmousedown;
         }
-      });
+      }else{
+        alert("Error 404 - URL not found");
+        window.location.replace("https://shortr.github.io");
+      }
+    });
   }
 };
+
+// for the rickrolling fun!
+function rhead(){
+  if(location.href.split("?")[1].split("&")[0] == "A" || location.href.split("?")[1].split("&")[0] == "llorkcir"){
+    const imgs = ["BV.png","FW.png","hmm.png","SH.png","TP.png"];
+    let m = "images/" + imgs[Math.floor(Math.random()*imgs.length)];
+    const fb = `<meta property="og:image:secure_url" content="https://shortr.github.io/${m}">
+    <meta property="og:image" content="http://shortr.github.io/${m}">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:url" content="https://shortr.github.io/?A">
+    <meta property="og:title" content="25 Top Tips For a Good Life!">
+    <meta property="og:description" content="Learn how to enjoy life to the fullest!">
+    <meta property="og:type" content="website">`;
+    const temp = document.createElement("template");
+    temp.innerHTML = fb;
+    var clones = temp.content.cloneNode(true);
+    document.head.append(clones);
+  }
+}
+rhead();
